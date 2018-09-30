0d6a3284de097edcb9362e55871aed77e7355f38
Peggy Li <xpeggyxli@gmail.com>
2018-09-30 02:33:12 -0400

    Restructure application

--- app/src/main/java/xyz/peggyli/streetsnap/MainActivity.java
+++ app/src/main/java/xyz/peggyli/streetsnap/MainActivity.java
@@ -1,183 +1,134 @@
 package xyz.peggyli.streetsnap;
 
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-import com.snapchat.kit.sdk.SnapLogin;
-import com.snapchat.kit.sdk.core.controller.LoginStateController;
-import com.snapchat.kit.sdk.login.models.MeData;
-import com.snapchat.kit.sdk.login.models.UserDataResponse;
-import com.snapchat.kit.sdk.login.networking.FetchUserDataCallback;
 
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout mDrawerLayout;
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    // Make sure to be using android.support.v7.app.ActionBarDrawerToggle version.
+    // The android.support.v4.app.ActionBarDrawerToggle has been deprecated.
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-            new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(MenuItem menuItem) {
-                    // set item as selected to persist highlight
-                    menuItem.setChecked(true);
-                    // close drawer when item is tapped
-                    mDrawerLayout.closeDrawers();
+        // Tie DrawerLayout events to the ActionBarToggle
+        mDrawer.addDrawerListener(drawerToggle);
 
-                    int id = menuItem.getItemId();
-                    switch(id)
-                    {
-                        case R.id.nav_map:
-                            // Create an Intent to start the second activity
-                            Intent randomIntent = new Intent(MainActivity.this, MapsActivity.class);
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nav_view);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
 
-                            // Start the new activity.
-                            startActivity(randomIntent);
-                            Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();
-                        default:
-                            return true;
-                    }
-                }
-        });
+        Fragment fragment = null;
+        Class fragmentClass = FeedFragment.class;
 
-        final TextView nameTextView = findViewById(R.id.textView);
-        final Button unlinkButton = findViewById(R.id.button2);
-        final ImageView avatarImageView = findViewById(R.id.imageView);
-        if (SnapLogin.isUserLoggedIn(this)) {
-            Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_LONG).show();
-
-
-            String query = "{me{bitmoji{avatar},displayName}}";
-            String variables = null;
-            SnapLogin.fetchUserData(this, query, null, new FetchUserDataCallback() {
-                @Override
-                public void onSuccess(@Nullable UserDataResponse userDataResponse) {
-                    if (userDataResponse == null || userDataResponse.getData() == null) {
-                        return;
-                    }
-
-                    MeData meData = userDataResponse.getData().getMe();
-                    if (meData == null) {
-                        return;
-                    }
-
-                    nameTextView.setText(userDataResponse.getData().getMe().getDisplayName());
-                    Toast.makeText(getApplicationContext(), "Display name : " + userDataResponse.getData().getMe().getDisplayName(), Toast.LENGTH_LONG).show();
-                    if (meData.getBitmojiData() != null) {
-                        Glide.with(getApplicationContext())
-                                .load(meData.getBitmojiData().getAvatar())
-                                .into(avatarImageView);
-                    }
-                }
-
-                @Override
-                public void onFailure(boolean isNetworkError, int statusCode) {
-
-                }
-            });
-
-        }else{
-            unlinkButton.setVisibility(View.INVISIBLE);
-            Toast.makeText(getApplicationContext(), "NOT Logged in", Toast.LENGTH_LONG).show();
-            View mLoginButton = SnapLogin.getButton(getApplicationContext(), (ViewGroup)findViewById(R.id.content_frame));
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        final LoginStateController.OnLoginStateChangedListener mLoginStateChangedListener =
-                new LoginStateController.OnLoginStateChangedListener() {
-                    @Override
-                    public void onLoginSucceeded() {
-                        // Here you could update UI to show login success
-                        Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_LONG).show();
-//                        recreate();
-//                        recreate();
-                        recreate();
-                    }
-
-                    @Override
-                    public void onLoginFailed() {
-                        // Here you could update UI to show login failure
-                    }
-
-                    @Override
-                    public void onLogout() {
-                        // Here you could update UI to reflect logged out state
-                        Toast.makeText(getApplicationContext(), "Logged out", Toast.LENGTH_LONG).show();
-                        recreate();
-                    }
-                };
-
-// Add the LoginStateChangedListener you’ve defined to receive LoginInState updates
-        SnapLogin.getLoginStateController(this).addOnLoginStateChangedListener(mLoginStateChangedListener);
-
-//        View buttonView = findViewById(R.id.button);
-//        buttonView.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick() {
-//                SnapLogin.getAuthTokenManager(getApplicationContext()).startTokenGrant();
-//            }
-//        });
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
     }
 
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE 1: Make sure to override the method with only a single `Bundle` argument
+    // Note 2: Make sure you implement the correct `onPostCreate(Bundle savedInstanceState)` method.
+    // There are 2 signatures and only `onPostCreate(Bundle state)` shows the hamburger icon.
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_feed:
+                fragmentClass = FeedFragment.class;
+                break;
+            case R.id.nav_map:
+                fragmentClass = MapFragment.class;
+                break;
+            default:
+                fragmentClass = FeedFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
-                return true;
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * Show a toast
-     * @param view -- the view that is clicked
-     */
-    public void showMap(View view){
-        // Create an Intent to start the second activity
-        Intent randomIntent = new Intent(this, MapsActivity.class);
-
-        // Start the new activity.
-        startActivity(randomIntent);
-    }
-
-    public void unlink(View view){
-        SnapLogin.getAuthTokenManager(this).revokeToken();
-        Toast.makeText(getApplicationContext(), "unlinking", Toast.LENGTH_LONG).show();
-//        finish();
-//        overridePendingTransition(0, 0);
-//        startActivity(getIntent());
-//        overridePendingTransition(0, 0);
-        recreate();
-    }
-
 }
---
+++ app/src/main/java/xyz/peggyli/streetsnap/MapFragment.java
@@ -0,0 +1,65 @@
+package xyz.peggyli.streetsnap;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if (mapFragment == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+        return v;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
