c3b6ea740996638378f01497a871a6a39f2ba1ce
Peggy Li <xpeggyxli@gmail.com>
2018-09-29 23:10:32 -0400

    Add nav panel

--- app/src/main/java/xyz/peggyli/streetsnap/MainActivity.java
+++ app/src/main/java/xyz/peggyli/streetsnap/MainActivity.java
@@ -2,17 +2,69 @@ package xyz.peggyli.streetsnap;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    // set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // close drawer when item is tapped
+                    mDrawerLayout.closeDrawers();
+
+                    int id = menuItem.getItemId();
+                    switch(id)
+                    {
+                        case R.id.nav_map:
+                            // Create an Intent to start the second activity
+                            Intent randomIntent = new Intent(MainActivity.this, MapsActivity.class);
+
+                            // Start the new activity.
+                            startActivity(randomIntent);
+                            Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();
+                        default:
+                            return true;
+                    }
+                }
+        });
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     /**
      * Show a toast
      * @param view -- the view that is clicked
